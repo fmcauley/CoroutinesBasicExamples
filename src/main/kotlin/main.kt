ada76780187ef65d6ca71a95282dfb1abdb8ae67
Frank McAuley <frank.mcauley@gmail.com>
2020-01-30 12:47:40 -0500

    FM - First example of Coroutine and Thread. The delay function does not block thread but suspends coroutine

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,21 @@
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 fun main() {
-    GlobalScope.launch{}
+    GlobalScope.launch{
+        delay(1000L)
+        print("World!")
+    }
+    println("Hello,")
+    Thread.sleep(2000L)
+
+    // Thread Example with no Coroutine
+    thread {
+        Thread.sleep(1000L)
+        print("Hot!")
+    }
+    println("Like it's")
+    Thread.sleep(2000L)
 }
