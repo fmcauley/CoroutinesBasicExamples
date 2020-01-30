dbf5ded0d48099782b296c956134f963e37391d1
Frank McAuley <frank.mcauley@gmail.com>
2020-01-30 12:55:05 -0500

    FM - using the runBlock at the top level. This works as an adaptor that is used to start the top-level main coroutin. A well formed main function in Kotlin must reutrn Unit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,15 +4,16 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
-fun main() {
+fun main() = runBlocking<Unit> {
     GlobalScope.launch {
         delay(1000L)
         println("World!")
     }
     println("Hello,")
-    runBlocking {
-        delay(2000L)
-    }
+    delay(2000L)
 
-    // The main thread invokes runBlocking blocks until the coroutine inside runBlock completes
+    /*
+    Here runBlocking<Unit> { ... } works as an adaptor that is used to start the top-level main coroutine.
+    We explicitly specify its Unit return type, because a well-formed main function in Kotlin has to return Unit.
+     */
 }
